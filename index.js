1f4633a7c13321da78562509acd24e8a5c061db6
Shuyibe48 <gthrough48@gmail.com>
2023-06-15 12:25:00 +0600

    server site connect with git

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const morgan = require('morgan')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const port = process.env.PORT || 5000
+
+
+// middleware
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Athlete\'s Haven Server is running..')
+})
+
+app.listen(port, () => {
+    console.log(`Athlete\'s Haven is running on port ${port}`)
+})
